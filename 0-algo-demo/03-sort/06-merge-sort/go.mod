e92ea8ecc381dc10bfebaf0b9dc9c6da6afc4546
hank <kslhy456@163.com>
2020-07-08 09:56:07 +0800

    归并，代码有问题

---
+++ 0-algo-demo/03-sort/06-merge-sort/go.mod
@@ -0,0 +1,3 @@
+module github.com/agocan/leetcode-demo/0-algo-demo/03-sort/06-merge-sort
+
+go 1.14
---
+++ 0-algo-demo/03-sort/06-merge-sort/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+// 归并, 此代码有问题，
+/*
+[1,3,5,7,9]
+[2,4,6,8]
+两个列表合并
+[1,3,5,7,9,2,4,6,8]
+low是左边列表的下标
+mid是左边列表的最右下标
+high是整个列表的最右下标
+[1,2,3,4,5,6,7,8,9]
+*/
+func merge(nums []int, low, mid, high int) {
+	i := low
+	j := mid + 1
+	var numsTmp []int
+	for i <= mid && j <= high {
+
+		if nums[i] < nums[j] {
+			numsTmp = append(numsTmp, nums[i])
+			i++
+		} else {
+			numsTmp = append(numsTmp, nums[j])
+			j++
+		}
+	}
+	for ; i <= mid; i++ {
+		numsTmp = append(numsTmp, nums[i])
+
+	}
+	for ; j <= high; j++ {
+		numsTmp = append(numsTmp, nums[j])
+
+	}
+	for k := low; k < len(numsTmp); k++ {
+		nums[k] = numsTmp[k]
+	}
+}
+
+func mergeSort(nums []int, low, high int) {
+	if low < high {
+
+		mid := (low + high) / 2
+		mergeSort(nums, low, mid)
+		mergeSort(nums, mid+1, high)
+		//fmt.Println(nums)
+		merge(nums, low, mid, high)
+
+	}
+
+}
+
+func main() {
+	nums := []int{1, 3, 5, 7, 9, 2, 3, 3, 4, 6, 8, 9, 10}
+	mergeSort(nums, 0, len(nums)-1)
+	fmt.Println(nums)
+}
