1022036ac846f30ed71daea3cc7e97201ca8df03
hank <kslhy456@163.com>
2020-07-10 15:24:24 +0800

    add 删除元素

--- 026-removeDuplicates/main.go
+++ 026-removeDuplicates/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 
@@ -14,6 +16,8 @@ package main
 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
 
 你不需要考虑数组中超出新长度后面的元素。
+
+
 示例 2:
 
 给定 nums = [0,0,1,1,1,2,2,3,3,4],
@@ -43,10 +47,25 @@ for (int i = 0; i < len; i++) {
 */
 
 func removeDuplicates(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	p := 0
+	q := 1
+	for q < len(nums) {
+		if nums[p] != nums[q] {
+			if q-p > 1 {
+				nums[p+1] = nums[q]
+			}
+			p++
+		}
+		q++
+	}
+	return p + 1
 
-	return -1
 }
 
 func main() {
-
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(nums))
 }
