c864f4f5667c61a7ea1e871675b511f9c922f27b
hank <kslhy456@163.com>
2020-07-13 20:13:18 +0800

    add 链表两数相加

--- 002-addTwoNumbers/main.go
+++ 002-addTwoNumbers/main.go
@@ -1,5 +1,8 @@
 package main
 
+// 还不会，过后再看
+import "fmt"
+
 /*
 
 中等
@@ -25,10 +28,76 @@ type listNode struct {
 }
 
 func addTwoNumbers(l1 *listNode, l2 *listNode) *listNode {
-
-	return nil
+	if l1 == nil && l2 == nil {
+		return nil
+	}
+	s := new(listNode) // &{0 <nil>} 所以必须有值
+	p := s             //
+	u := 0             // 进1位置， 例如9+3=12， 进一位为 1
+	for ; l1 != nil && l2 != nil; l1, l2 = l1.Next, l2.Next {
+		p.Val = (l1.Val + l2.Val + u/10) % 10 // 先算个位数，9 + 8 = 17 求余
+		u = l1.Val + l2.Val + u/10 - p.Val    // u = 1
+		if l1.Next != nil && l2.Next != nil {
+			p.Next = new(listNode) // 如果有下一位数字，则继续开辟一个空间
+			p = p.Next
+		}
+	}
+	// 判断，如果参数都是为nil，就已经可以直接退出了。在第一行
+	if l1 == nil && l2 == nil && u == 0 {
+		// 没有任何
+		return s
+	} else if l1 == nil && l2 == nil {
+		// 同为3位数，然后都加完了。
+		p.Next = new(listNode)
+	} else if l1 == nil {
+		// 下面两个把剩余的数字给加上
+		p.Next = l2
+	} else if l2 == nil {
+		p.Next = l1
+	}
+	p = p.Next
+	for tmp := 0; u != 0 && p != nil; p = p.Next {
+		tmp = p.Val + u/10
+		p.Val = (p.Val + u/10) % 10
+		u = tmp - p.Val
+		if p.Next == nil && u != 0 {
+			p.Next = new(listNode)
+			p.Next.Val = u / 10
+			break
+		}
+	}
+	return s
 }
 
 func main() {
-
+	var l1 = new(listNode)
+	var l2 = new(listNode)
+	l1 = &listNode{
+		Val: 2,
+		Next: &listNode{
+			Val: 4,
+			Next: &listNode{
+				Val: 3,
+				Next: &listNode{
+					Val:  0,
+					Next: nil,
+				},
+			},
+		},
+	}
+	l2 = &listNode{
+		Val: 5,
+		Next: &listNode{
+			Val: 6,
+			Next: &listNode{
+				Val: 4,
+				Next: &listNode{
+					Val:  0,
+					Next: nil,
+				},
+			},
+		},
+	}
+	s := addTwoNumbers(l1, l2)
+	fmt.Println(s.Next.Next.Next.Val, s.Next.Next.Val, s.Next.Val, s.Val)
 }
